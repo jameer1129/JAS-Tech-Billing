90f86000e26b504456bed47850280a81cc45fe01
Jameer Ali Shaik <shaikjameerali2003@gmail.com>
2026-09-18 00:12:04 +0530

    fix loaders

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
    Safe Activation Handoff
    ========================================================= */
 
-const CACHE_NAME = "v2.2.0";
+const CACHE_NAME = "v2.1.3";
 
 // Delay before taking control of already-open pages.
 const CLAIM_DELAY_MS = 2000;
